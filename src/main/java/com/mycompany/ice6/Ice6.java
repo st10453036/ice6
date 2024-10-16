3daf301c9ab8ee1fa4a9d4ab386316078956fd2b
RC_Student_lab <RC_Student_lab@10.0.0.128>
2024-10-16 19:35:48 +0000

    2nd att

--- src/main/java/com/mycompany/ice6/Ice6.java
+++ src/main/java/com/mycompany/ice6/Ice6.java
@@ -11,6 +11,12 @@ package com.mycompany.ice6;
 public class Ice6 {
 
     public static void main(String[] args) {
-      
+               String sentence = "Hello, welcome to the Java programming ";
+        int numberOfVowels = countVowels(sentence);
+ 
+        System.out.println("The number of vowels in the sentence is: " + numberOfVowels);
+ 
+        
+
     }
 }
